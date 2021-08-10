b4fded08a4cb1a0eddf766bbabce2ca3fe941d34
Karina Izo <81497367+karinaizo@users.noreply.github.com>
2021-08-09 22:55:08 -0300

    Update Simulador de Consumo

---
+++ simulador.js
@@ -0,0 +1,60 @@
+function avancar1() {
+    document.getElementById('one').style.backgroundColor = 'white';
+    document.getElementById('two').style.backgroundColor = '#BA68C8';
+
+    document.getElementsByClassName('display')[0].style.display = 'none';
+    document.getElementsByClassName('display')[1].style.display = 'none';
+
+    document.getElementsByClassName('vez')[0].style.display = 'none';
+    document.getElementsByClassName('vez')[2].style.display = 'none';
+    document.getElementsByClassName('vez')[4].style.display = 'none';
+    document.getElementsByClassName('vez')[5].style.display = 'none';
+
+    document.getElementsByClassName('min')[0].style.display = 'block';
+    document.getElementsByClassName('min')[1].style.display = 'block';
+    document.getElementsByClassName('min')[2].style.display = 'block';
+    document.getElementsByClassName('min')[3].style.display = 'block';
+
+    document.getElementById('button').style.display = 'none';
+    document.getElementsByClassName('btn')[0].style.display = 'block';
+} 
+
+function avancar2() {
+    document.getElementById('two').style.backgroundColor = 'white';
+    document.getElementById('three').style.backgroundColor = '#BA68C8';
+
+    document.getElementById('formulario-img').style.display = 'none';
+    document.getElementById('categorias').style.display = 'flex';
+
+    document.getElementsByClassName('btn')[0].style.display = 'none';
+    document.getElementsByClassName('btn')[1].style.display = 'block';
+} 
+
+function calcular() {
+    document.getElementById('three').style.backgroundColor = 'white';
+    document.getElementById('four').style.backgroundColor = '#BA68C8';
+
+    document.getElementById('categorias').style.display = 'none';
+
+    document.getElementsByClassName('vez')[1].style.display = 'none';
+    document.getElementsByClassName('vez')[3].style.display = 'none';
+
+    document.getElementsByClassName('min')[0].style.display = 'none';
+    document.getElementsByClassName('min')[1].style.display = 'none';
+    document.getElementsByClassName('min')[2].style.display = 'none';
+    document.getElementsByClassName('min')[3].style.display = 'none';
+
+    document.getElementsByClassName('display')[0].style.display = 'block';
+    document.getElementsByClassName('display')[1].style.display = 'block';
+    document.getElementById('formulario-img').style.display = 'flex';
+
+    document.getElementsByClassName('number-input')[0].style.display = 'flex';
+    document.getElementsByClassName('number-input')[1].style.display = 'flex';
+    document.getElementsByClassName('number-input')[2].style.display = 'flex';
+    document.getElementsByClassName('number-input')[3].style.display = 'flex';
+    document.getElementsByClassName('number-input')[4].style.display = 'flex';
+    document.getElementsByClassName('number-input')[5].style.display = 'flex';
+
+    document.getElementsByClassName('btn')[1].style.display = 'none';
+    document.getElementsByClassName('btn')[2].style.display = 'block';
+} 
