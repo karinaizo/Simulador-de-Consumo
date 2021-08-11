8beaa0d32183991dbd0c54b2a28a5232c052a1a7
Karina Izo <81497367+karinaizo@users.noreply.github.com>
2021-08-11 14:16:47 -0300

    Update Simulador de Consumo

--- simulador.js
+++ simulador.js
@@ -101,18 +101,44 @@ function calcular() {
         var min2 = (document.getElementById('input2').value)*vez4;
         var min3 = (document.getElementById('input3').value)*vez5;
 
-        document.getElementById('res0').value = min0.toFixed(2).replace('.', ",") + ' min';
-        document.getElementById('res1').value = (min0*2.4).toFixed(2).replace('.', ",") + ' L'; 
-        document.getElementById('res3').value = '--- min';
-        document.getElementById('res4').value = (vez1*12).toFixed(2).replace('.', ',') + ' L';
-        document.getElementById('res6').value = min1.toFixed(2).replace('.', ",") + ' min';
-        document.getElementById('res7').value = (min1*9).toFixed(2).replace('.', ",") + ' L';
-        document.getElementById('res9').value = '--- min';
-        document.getElementById('res10').value = (vez3*135).toFixed(2).replace('.', ',') + ' L';
-        document.getElementById('res12').value = min2.toFixed(2).replace('.', ",") + ' min';
-        document.getElementById('res13').value = (min2*7.8).toFixed(2).replace('.', ",") + ' L';
-        document.getElementById('res15').value = min3.toFixed(2).replace('.', ",") + ' min';
-        document.getElementById('res16').value = (min3*2.5).toFixed(2).replace('.', ",") + ' L';
+        var res0 = document.getElementById('res0');
+        var res1 = document.getElementById('res1');
+        var res3 = document.getElementById('res3');
+        var res4 = document.getElementById('res4');
+        var res6 = document.getElementById('res6');
+        var res7 = document.getElementById('res7');
+        var res9 = document.getElementById('res9');
+        var res10 = document.getElementById('res10');
+        var res12 = document.getElementById('res12');
+        var res13 = document.getElementById('res13');
+        var res15 = document.getElementById('res15');
+        var res16 = document.getElementById('res16');
+
+        res0.style.backgroundColor = 'white';
+        res1.style.backgroundColor = 'white';
+        res3.style.backgroundColor = 'white';
+        res4.style.backgroundColor = 'white';
+        res6.style.backgroundColor = 'white';
+        res7.style.backgroundColor = 'white';
+        res9.style.backgroundColor = 'white';
+        res10.style.backgroundColor = 'white';
+        res12.style.backgroundColor = 'white';
+        res13.style.backgroundColor = 'white';
+        res15.style.backgroundColor = 'white';
+        res16.style.backgroundColor = 'white';
+
+        res0.value = min0.toFixed(2).replace('.', ",") + ' min';
+        res1.value = (min0*2.4).toFixed(2).replace('.', ",") + ' L'; 
+        res3.value = vez1 + ' vez(es)';
+        res4.value = (vez1*12).toFixed(2).replace('.', ',') + ' L';
+        res6.value = min1.toFixed(2).replace('.', ",") + ' min';
+        res7.value = (min1*9).toFixed(2).replace('.', ",") + ' L';
+        res9.value = vez3 + ' vez(es)';
+        res10.value = (vez3*135).toFixed(2).replace('.', ',') + ' L';
+        res12.value = min2.toFixed(2).replace('.', ",") + ' min';
+        res13.value = (min2*7.8).toFixed(2).replace('.', ",") + ' L';
+        res15.value = min3.toFixed(2).replace('.', ",") + ' min';
+        res16.value = (min3*2.5).toFixed(2).replace('.', ",") + ' L';
 
         var litros0 = min0*2.4;
         var litros1 = vez1*12;
@@ -122,138 +148,158 @@ function calcular() {
         var litros5 = min3*2.5;
 
         var totalmin=(min0+min1+min2+min3);
-        var totalitros=(litros0+litros1+litros2+litros3+litros4+litros5);
+        var totallitros=(litros0+litros1+litros2+litros3+litros4+litros5);
 
-        // if (((totalitros/1000) >=0) && ((totalitros/1000) <=10)) {
-        //     if (document.getElementById('residencial-comum').checked == true) {
-        //         var reais0 = (litros0*29)/totallitros;
-        //         var reais1 = (litros1*29)/totalitros;
-        //         var reais2 = (litros2*29)/totalitros;
-        //         var reais3 = (litros3*29)/totalitros;
-        //         var reais4 = (litros4*29)/totalitros;
-        //         var reais5 = (litros5*29)/totalitros;
-        //         var totalvalor = 29;
-        //     }else if (document.getElementById('residencial-social').checked == true) {
-        //         var reais0 = (litros0*9.05)/totalitros;
-        //         var reais1 = (litros1*9.05)/totalitros;
-        //         var reais2 = (litros2*9.05)/totalitros;
-        //         var reais3 = (litros3*9.05)/totalitros;
-        //         var reais4 = (litros4*9.05)/totalitros;
-        //         var reais5 = (litros5*9.05)/totalitros;
-        //         var totalvalor = 9.05;
-        //     }else {
-        //         var reais0 = (litros0*6.90)/totalitros;
-        //         var reais1 = (litros1*6.90)/totalitros;
-        //         var reais2 = (litros2*6.90)/totalitros;
-        //         var reais3 = (litros3*6.90)/totalitros;
-        //         var reais4 = (litros4*6.90)/totalitros;
-        //         var reais5 = (litros5*6.90)/totalitros;
-        //         var totalvalor = 6.90;
-        //     }
+        if (((totallitros/1000) >=0) && ((totallitros/1000) <=10)) {
+            if (document.getElementById('residencial-comum').checked == true) {
+                var reais0 = (litros0*29)/totallitros;
+                var reais1 = (litros1*29)/totallitros;
+                var reais2 = (litros2*29)/totallitros;
+                var reais3 = (litros3*29)/totallitros;
+                var reais4 = (litros4*29)/totallitros;
+                var reais5 = (litros5*29)/totallitros;
+                var totalvalor = 29;
+            }else if (document.getElementById('residencial-social').checked == true) {
+                var reais0 = (litros0*9.05)/totallitros;
+                var reais1 = (litros1*9.05)/totallitros;
+                var reais2 = (litros2*9.05)/totallitros;
+                var reais3 = (litros3*9.05)/totallitros;
+                var reais4 = (litros4*9.05)/totallitros;
+                var reais5 = (litros5*9.05)/totallitros;
+                var totalvalor = 9.05;
+            }else {
+                var reais0 = (litros0*6.90)/totallitros;
+                var reais1 = (litros1*6.90)/totallitros;
+                var reais2 = (litros2*6.90)/totallitros;
+                var reais3 = (litros3*6.90)/totallitros;
+                var reais4 = (litros4*6.90)/totallitros;
+                var reais5 = (litros5*6.90)/totallitros;
+                var totalvalor = 6.90;
+            }
         
-        // }else if ((totalitros/1000) >=11 && (totalitros/1000) <=20) {
-        //     if (document.getElementById('residencial-comum').checked == true) {
-        //         var reais0 = (litros0/1000)*4.54;
-        //         var reais1 = (litros1/1000)*4.54;
-        //         var reais2 = (litros2/1000)*4.54;
-        //         var reais3 = (litros3/1000)*4.54;
-        //         var reais4 = (litros4/1000)*4.54;
-        //         var reais5 = (litros5/1000)*4.54;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }else if (document.getElementById('residencial-social').checked == true) {
-        //         var reais0 = (litros0/1000)*1.55;
-        //         var reais1 = (litros1/1000)*1.55;
-        //         var reais2 = (litros2/1000)*1.55;
-        //         var reais3 = (litros3/1000)*1.55;
-        //         var reais4 = (litros4/1000)*1.55;
-        //         var reais5 = (litros5/1000)*1.55;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }else {
-        //         var reais0 = (litros0/1000)*0.78;
-        //         var reais1 = (litros1/1000)*0.78;
-        //         var reais2 = (litros2/1000)*0.78;
-        //         var reais3 = (litros3/1000)*0.78;
-        //         var reais4 = (litros4/1000)*0.78;
-        //         var reais5 = (litros5/1000)*0.78;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }
+        }else if ((totallitros/1000) >=11 && (totallitros/1000) <=20) {
+            if (document.getElementById('residencial-comum').checked == true) {
+                var reais0 = (litros0/1000)*4.54;
+                var reais1 = (litros1/1000)*4.54;
+                var reais2 = (litros2/1000)*4.54;
+                var reais3 = (litros3/1000)*4.54;
+                var reais4 = (litros4/1000)*4.54;
+                var reais5 = (litros5/1000)*4.54;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }else if (document.getElementById('residencial-social').checked == true) {
+                var reais0 = (litros0/1000)*1.55;
+                var reais1 = (litros1/1000)*1.55;
+                var reais2 = (litros2/1000)*1.55;
+                var reais3 = (litros3/1000)*1.55;
+                var reais4 = (litros4/1000)*1.55;
+                var reais5 = (litros5/1000)*1.55;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }else {
+                var reais0 = (litros0/1000)*0.78;
+                var reais1 = (litros1/1000)*0.78;
+                var reais2 = (litros2/1000)*0.78;
+                var reais3 = (litros3/1000)*0.78;
+                var reais4 = (litros4/1000)*0.78;
+                var reais5 = (litros5/1000)*0.78;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }
         
-        // }else if ((totalitros/1000) >=21 && (totalitros/1000 <=30)){
-        //     if (document.getElementById('residencial-comum').checked == true) {
-        //         var reais0 = (litros0/1000)*11.33;
-        //         var reais1 = (litros1/1000)*11.33;
-        //         var reais2 = (litros2/1000)*11.33;
-        //         var reais3 = (litros3/1000)*11.33;
-        //         var reais4 = (litros4/1000)*11.33;
-        //         var reais5 = (litros5/1000)*11.33;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }else if (document.getElementById('residencial-social').checked == true) {
-        //         var reais0 = (litros0/1000)*5.53;
-        //         var reais1 = (litros1/1000)*5.53;
-        //         var reais2 = (litros2/1000)*5.53;
-        //         var reais3 = (litros3/1000)*5.53;
-        //         var reais4 = (litros4/1000)*5.53;
-        //         var reais5 = (litros5/1000)*5.53;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }else {
-        //         var reais0 = (litros0/1000)*2.61;
-        //         var reais1 = (litros1/1000)*2.61;
-        //         var reais2 = (litros2/1000)*2.61;
-        //         var reais3 = (litros3/1000)*2.61;
-        //         var reais4 = (litros4/1000)*2.61;
-        //         var reais5 = (litros5/1000)*2.61;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }
+        }else if ((totallitros/1000) >=21 && (totallitros/1000 <=30)){
+            if (document.getElementById('residencial-comum').checked == true) {
+                var reais0 = (litros0/1000)*11.33;
+                var reais1 = (litros1/1000)*11.33;
+                var reais2 = (litros2/1000)*11.33;
+                var reais3 = (litros3/1000)*11.33;
+                var reais4 = (litros4/1000)*11.33;
+                var reais5 = (litros5/1000)*11.33;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }else if (document.getElementById('residencial-social').checked == true) {
+                var reais0 = (litros0/1000)*5.53;
+                var reais1 = (litros1/1000)*5.53;
+                var reais2 = (litros2/1000)*5.53;
+                var reais3 = (litros3/1000)*5.53;
+                var reais4 = (litros4/1000)*5.53;
+                var reais5 = (litros5/1000)*5.53;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }else {
+                var reais0 = (litros0/1000)*2.61;
+                var reais1 = (litros1/1000)*2.61;
+                var reais2 = (litros2/1000)*2.61;
+                var reais3 = (litros3/1000)*2.61;
+                var reais4 = (litros4/1000)*2.61;
+                var reais5 = (litros5/1000)*2.61;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }
         
-        // }else if ((totalitros/1000) >=31 && (totalitros/1000 <=50)) {
-        //     if (document.getElementById('residencial-comum').checked == true) {
-        //         var reais0 = (litros0/1000)*11.33;
-        //         var reais1 = (litros1/1000)*11.33;
-        //         var reais2 = (litros2/1000)*11.33;
-        //         var reais3 = (litros3/1000)*11.33;
-        //         var reais4 = (litros4/1000)*11.33;
-        //         var reais5 = (litros5/1000)*11.33;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }else {
-        //         var reais0 = (litros0/1000)*7.88;
-        //         var reais1 = (litros1/1000)*7.88;
-        //         var reais2 = (litros2/1000)*7.88;
-        //         var reais3 = (litros3/1000)*7.88;
-        //         var reais4 = (litros4/1000)*7.88;
-        //         var reais5 = (litros5/1000)*7.88;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }
+        }else if ((totallitros/1000) >=31 && (totallitros/1000 <=50)) {
+            if (document.getElementById('residencial-comum').checked == true) {
+                var reais0 = (litros0/1000)*11.33;
+                var reais1 = (litros1/1000)*11.33;
+                var reais2 = (litros2/1000)*11.33;
+                var reais3 = (litros3/1000)*11.33;
+                var reais4 = (litros4/1000)*11.33;
+                var reais5 = (litros5/1000)*11.33;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }else {
+                var reais0 = (litros0/1000)*7.88;
+                var reais1 = (litros1/1000)*7.88;
+                var reais2 = (litros2/1000)*7.88;
+                var reais3 = (litros3/1000)*7.88;
+                var reais4 = (litros4/1000)*7.88;
+                var reais5 = (litros5/1000)*7.88;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }
 
-        // }else {
-        //     if (document.getElementById('residencial-comum').checked == true) {
-        //         var reais0 = (litros0/1000)*12.48;
-        //         var reais1 = (litros1/1000)*12.48;
-        //         var reais2 = (litros2/1000)*12.48;
-        //         var reais3 = (litros3/1000)*12.48;
-        //         var reais4 = (litros4/1000)*12.48;
-        //         var reais5 = (litros5/1000)*12.48;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }else {
-        //         var reais0 = (litros0/1000)*8.71;
-        //         var reais1 = (litros1/1000)*8.71;
-        //         var reais2 = (litros2/1000)*8.71;
-        //         var reais3 = (litros3/1000)*8.71;
-        //         var reais4 = (litros4/1000)*8.71;
-        //         var reais5 = (litros5/1000)*8.71;
-        //         var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
-        //     }
-        // }
+        }else {
+            if (document.getElementById('residencial-comum').checked == true) {
+                var reais0 = (litros0/1000)*12.48;
+                var reais1 = (litros1/1000)*12.48;
+                var reais2 = (litros2/1000)*12.48;
+                var reais3 = (litros3/1000)*12.48;
+                var reais4 = (litros4/1000)*12.48;
+                var reais5 = (litros5/1000)*12.48;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }else {
+                var reais0 = (litros0/1000)*8.71;
+                var reais1 = (litros1/1000)*8.71;
+                var reais2 = (litros2/1000)*8.71;
+                var reais3 = (litros3/1000)*8.71;
+                var reais4 = (litros4/1000)*8.71;
+                var reais5 = (litros5/1000)*8.71;
+                var totalvalor = (reais0+reais1+reais2+reais3+reais4+reais5);
+            }
+        }
 
-        // window.document.getElementById('res18').value = totalmin.toFixed(2).replace('.', ',') + ' min';
-        // window.document.getElementById('res19').value = totalitros.toFixed(2).replace('.', ',') + ' L';
-        // window.document.getElementById('res20').value = 'R$' + totalvalor.toFixed(2).replace('.', ',');
+        var res2 = document.getElementById('res2');
+        var res5 = document.getElementById('res5');
+        var res8 = document.getElementById('res8');
+        var res11 = document.getElementById('res11');
+        var res14 = document.getElementById('res14');
+        var res17 = document.getElementById('res17');
+        var res18 = document.getElementById('res18');
+        var res19 = document.getElementById('res19');
+        var res20 = document.getElementById('res20');
 
-        // window.document.getElementById('res2').value= 'R$' + reais0.toFixed(2).replace('.', ',');
-        // window.document.getElementById('res5').value= 'R$' + reais1.toFixed(2).replace('.', ',');
-        // window.document.getElementById('res8').value = 'R$' + reais2.toFixed(2).replace('.', ',');
-        // window.document.getElementById('res11').value = 'R$' + reais3.toFixed(2).replace('.', ',');
-        // window.document.getElementById('res14').value = 'R$' + reais4.toFixed(2).replace('.', ',');
-        // window.document.getElementById('res17').value = 'R$' + reais5.toFixed(2).replace('.', ',');
+        res2.style.backgroundColor = 'white';
+        res5.style.backgroundColor = 'white';
+        res8.style.backgroundColor = 'white';
+        res11.style.backgroundColor = 'white';
+        res14.style.backgroundColor = 'white';
+        res17.style.backgroundColor = 'white';
+        res18.style.backgroundColor = 'white';
+        res19.style.backgroundColor = 'white';
+        res20.style.backgroundColor = 'white';
+
+        res18.value = totalmin.toFixed(2).replace('.', ',') + ' min';
+        res19.value = totallitros.toFixed(2).replace('.', ',') + ' L';
+        res20.value = 'R$' + totalvalor.toFixed(2).replace('.', ',');
+
+        res2.value= 'R$' + reais0.toFixed(2).replace('.', ',');
+        res5.value= 'R$' + reais1.toFixed(2).replace('.', ',');
+        res8.value = 'R$' + reais2.toFixed(2).replace('.', ',');
+        res11.value = 'R$' + reais3.toFixed(2).replace('.', ',');
+        res14.value = 'R$' + reais4.toFixed(2).replace('.', ',');
+        res17.value = 'R$' + reais5.toFixed(2).replace('.', ',');
 
         document.getElementsByClassName('display')[0].style.display = 'block';
         document.getElementsByClassName('display')[1].style.display = 'block';
